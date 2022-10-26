4386e51aefa40503cdf720514aa742179c70583d
Youngwook-Go <113055584+Youngwook-Go@users.noreply.github.com>
2022-10-26 18:26:10 +0000

    z

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Youngwook All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Youngwook
+// Created on: OCT 2022
 // This file contains the JS functions for index.html
 
 "use strict"
@@ -10,14 +10,25 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS2O-Unit3-01-JS/sw.js", {
+    scope: "/ICS2O-Unit3-01-JS/",
   })
 }
 
 /**
  * This function displays an alert.
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+
+function submit() {
+  // input
+  const baseA = parseFloat(document.getElementById("base-value-a").value)
+  const baseB = parseFloat(document.getElementById("base-value-b").value)
+  const height = parseFloat(document.getElementById("height-value").value)
+  
+  // process
+  const area = (baseA + baseB) / 2 * height
+
+  // output
+  document.getElementById("area").innerHTML =
+    "Area of the trapzoid is: " + area.toFixed(2)
 }
